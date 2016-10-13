de87a1b3dc88a4c04a8b995be448e5d1d282b026
isuru <isuru@research>
2016-10-13 07:59:17 +0530

    commit first start of the project now have to add command analysis to other way.

--- src/pack_analy_resrh/Pack_Analy_Resrh.java
+++ src/pack_analy_resrh/Pack_Analy_Resrh.java
@@ -1,9 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pack_analy_resrh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -15,7 +14,22 @@ public class Pack_Analy_Resrh {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String tmp;
+        Process p;
+        BufferedReader br;
+        
+        try{
+            p = Runtime.getRuntime().exec("ls -l");
+            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            
+            while ((tmp = br.readLine())!= null) {                
+                System.out.println(tmp);
+            }
+            
+        }catch(IOException e){
+            System.out.println("Problem with command");
+        }
     }
     
 }
